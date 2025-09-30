8eea690c0fef64f66b455a34e05aa5ab9691f6c5
ipsilonmain <ipsilonmain@gmail.com>
2025-09-30 16:22:02 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,23 @@
-// Nahraď hodnotami ze svého Firebase projektu
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyDaXCsrLdejMBWznEoeiaOkxEFyRWWK1RI",
     authDomain: "trida-kasa.firebaseapp.com",
     projectId: "trida-kasa",
     storageBucket: "trida-kasa.firebasestorage.app",
     messagingSenderId: "882167279633",
-    appId: "1:882167279633:web:ba5f62338ff290e01241b0"
-};
+    appId: "1:882167279633:web:ba5f62338ff290e01241b0",
+    measurementId: "G-RQE5PYP8ME"
+  };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
