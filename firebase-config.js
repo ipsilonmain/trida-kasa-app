adc4242f1941fb11ab7f2c35560aa47b9b7c8ae6
ipsilonmain <ipsilonmain@gmail.com>
2025-09-30 16:27:37 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,19 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDaXCsrLdejMBWznEoeiaOkxEFyRWWK1RI",
-    authDomain: "trida-kasa.firebaseapp.com",
-    projectId: "trida-kasa",
-    storageBucket: "trida-kasa.firebasestorage.app",
-    messagingSenderId: "882167279633",
-    appId: "1:882167279633:web:ba5f62338ff290e01241b0",
-    measurementId: "G-RQE5PYP8ME"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDaXCsrLdejMBWznEoeiaOkxEFyRWWK1RI",
+  authDomain: "trida-kasa.firebaseapp.com",
+  projectId: "trida-kasa",
+  storageBucket: "trida-kasa.firebasestorage.app",
+  messagingSenderId: "882167279633",
+  appId: "1:882167279633:web:ba5f62338ff290e01241b0",
+  measurementId: "G-RQE5PYP8ME"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
